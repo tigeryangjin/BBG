e4faf74095df2719e35e46e13015ef25b9c42025
yangjin <tigeryangjin@gmail.com>
2015-02-11 19:25:03 +0800

    Revert "upload"
    This reverts commit 15a55e2ce27ea4e47c89101d9fc435e6d8606d8d.

--- RA/BUG/W_RTL_ITEM_GRP1_D/p14342982_132_Generic/Readme.txt
+++
@@ -1,7 +0,0 @@
-Bug Id:        14342982
-                   
-Bug Extract:   UDA FIELDS HAVE PERFORMANCE ISSUE.
-
-
-Module:		                                             Revision 
-POP_SIL_RetailItemUDADimensionLoad.xml                           1
--- RA/odiPurgeAllCache.sh
+++
@@ -1,6 +0,0 @@
-export ORACLE_HOME=$MW_HOME/Oracle_BI1
-export ODBCINI=/u01/app/oracle/product/11.1.0/obiee_1/instances/instance1/bifoundation/OracleBIApplication/coreapplication/setup/odbc.ini
-cd /u01/app/oracle/product/11.1.0/obiee_1/instances/instance1/bifoundation/OracleBIApplication/coreapplication/setup/
-./bi-init.sh
-cd /u01/app/oracle/product/11.1.0/obiee_1/Oracle_BI1/bifoundation/server/bin/
-nqcmd -d AnalyticsWeb -u weblogic -p Oracle123 -s /u01/app/oracle/product/11.1.0/obiee_1/instances/instance1/SanFu/purgeAllCache >> /u01/app/oracle/product/11.1.0/obiee_1/instances/instance1/SanFu/NQCMD.log
