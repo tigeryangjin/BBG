4716b0fdaa36d875b08ac96571b4f8d128d1c1af
yangjin <tigeryangjin@gmail.com>
2015-07-08 08:48:17 +0800

    upload

--- Task/2015-05-18_BBG_ETL_MONTH_DATA改写月汇总_MERGE/2015-07-01_BBG_ETL_MONTH_DATA报错ORA-08103处理.sql
+++ Task/2015-05-18_BBG_ETL_MONTH_DATA改写月汇总_MERGE/2015-07-01_BBG_ETL_MONTH_DATA报错ORA-08103处理.sql
@@ -1,23 +1,57 @@
 --1.
-SELECT T.owner,
-       T.table_name,
-       T.inmemory,
-       T.inmemory_priority,
-       T.inmemory_distribute,
-       T.inmemory_compression,
-       T.inmemory_duplicate
+SELECT T.OWNER,
+       T.TABLE_NAME,
+       T.INMEMORY,
+       T.INMEMORY_PRIORITY,
+       T.INMEMORY_DISTRIBUTE,
+       T.INMEMORY_COMPRESSION,
+       T.INMEMORY_DUPLICATE
   FROM ALL_ALL_TABLES T
- WHERE T.inmemory = 'ENABLED'
-   AND T.table_name LIKE '%MN%'
- ORDER BY T.inmemory, T.owner, T.table_name;
+ WHERE /*T.INMEMORY = 'ENABLED'
+   AND*/
+ T.TABLE_NAME IN ('BBG_RA_SLS_SC_LC_DY_A',
+                  'W_RTL_SLS_CL_LC_DY_A',
+                  'W_RTL_SLS_CL_LC_DY_CUR_A',
+                  'W_RTL_SLS_DP_LC_DY_A',
+                  'W_RTL_SLS_DP_LC_DY_CUR_A',
+                  'W_RTL_SLS_LC_DY_A',
+                  'W_RTL_SLS_SC_DY_A',
+                  'W_RTL_SLS_SC_DY_CUR_A',
+                  'W_RTL_SLS_SC_LC_DY_A',
+                  'W_RTL_SLS_SC_LC_DY_CUR_A')
+ ORDER BY T.INMEMORY, T.OWNER, T.TABLE_NAME;
 
+EXECUTE IMMEDIATE 'ALTER TABLE RADM.W_RTL_SLS_SC_LC_DY_CUR_A NO INMEMORY';
 --2.NO INMEMORY
-SELECT 'ALTER TABLE ' || T.owner || '.' || T.table_name || ' NO INMEMORY;'
+SELECT 'EXECUTE IMMEDIATE ''ALTER TABLE ' || T.OWNER || '.' || T.TABLE_NAME ||
+       ' NO INMEMORY'';'
   FROM ALL_ALL_TABLES T
- WHERE T.inmemory = 'ENABLED';
+ WHERE T.INMEMORY = 'ENABLED'
+   AND T.TABLE_NAME IN ('BBG_RA_SLS_SC_LC_DY_A',
+                        'W_RTL_SLS_CL_LC_DY_A',
+                        'W_RTL_SLS_CL_LC_DY_CUR_A',
+                        'W_RTL_SLS_DP_LC_DY_A',
+                        'W_RTL_SLS_DP_LC_DY_CUR_A',
+                        'W_RTL_SLS_LC_DY_A',
+                        'W_RTL_SLS_SC_DY_A',
+                        'W_RTL_SLS_SC_DY_CUR_A',
+                        'W_RTL_SLS_SC_LC_DY_A',
+                        'W_RTL_SLS_SC_LC_DY_CUR_A')
+ ORDER BY T.TABLE_NAME;
 
 --3.INMEMORY
-SELECT 'ALTER TABLE ' || T.owner || '.' || T.table_name ||
-       ' INMEMORY PRIORITY CRITICAL MEMCOMPRESS FOR CAPACITY HIGH;'
+SELECT 'EXECUTE IMMEDIATE ''ALTER TABLE ' || T.OWNER || '.' || T.TABLE_NAME ||
+       ' INMEMORY PRIORITY CRITICAL MEMCOMPRESS FOR CAPACITY HIGH'';'
   FROM ALL_ALL_TABLES T
- WHERE T.inmemory = 'DISABLED';
+ WHERE T.INMEMORY = 'ENABLED'
+   AND T.TABLE_NAME IN ('BBG_RA_SLS_SC_LC_DY_A',
+                        'W_RTL_SLS_CL_LC_DY_A',
+                        'W_RTL_SLS_CL_LC_DY_CUR_A',
+                        'W_RTL_SLS_DP_LC_DY_A',
+                        'W_RTL_SLS_DP_LC_DY_CUR_A',
+                        'W_RTL_SLS_LC_DY_A',
+                        'W_RTL_SLS_SC_DY_A',
+                        'W_RTL_SLS_SC_DY_CUR_A',
+                        'W_RTL_SLS_SC_LC_DY_A',
+                        'W_RTL_SLS_SC_LC_DY_CUR_A')
+ ORDER BY T.TABLE_NAME;
