5f22b4c02a95ed3289af22fe6f436cd9bd4a0591
tigeryangjin <tigeryangjin@gmail.com>
2015-09-28 20:10:37 +0800

    backup

--- RMS-RA Data check/RMS-RA部门销售数据核对/SQL/RA-JL数据对比.sql
+++ RMS-RA Data check/RMS-RA部门销售数据核对/SQL/RA-JL数据对比.sql
@@ -4,10 +4,10 @@ select T.DAY_DT,
               INSTR(P.PROD_CAT5, '~', 1, 3) -
               (INSTR(P.PROD_CAT5, '~', 1, 2) + 1)) DEPT,
        SUM(T.SLS_QTY - T.RET_QTY) QTY,
-       SUM((T.SLS_AMT_LCL - T.RET_AMT_LCL) -
-           (T.SLS_TAX_AMT_LCL - T.RET_TAX_AMT_LCL)) NOTAX_AMT,
-       SUM(((T.SLS_AMT_LCL - T.RET_AMT_LCL) -
-           (T.SLS_TAX_AMT_LCL - T.RET_TAX_AMT_LCL)) -
+       SUM((T.SLS_AMT_LCL - T.SLS_TAX_AMT_LCL) -
+           (T.RET_AMT_LCL - T.RET_TAX_AMT_LCL)) NOTAX_AMT,
+       SUM(((T.SLS_AMT_LCL - T.SLS_TAX_AMT_LCL) -
+           (T.RET_AMT_LCL - T.RET_TAX_AMT_LCL)) -
            (T.SLS_PROFIT_AMT_LCL - T.RET_PROFIT_AMT_LCL)) COST
   from (SELECT SLS_TRX_ID,
                PROD_IT_NUM,
@@ -34,7 +34,7 @@ select T.DAY_DT,
                SLS_MANUAL_MKUP_AMT_LCL,
                -RET_MANUAL_MKDN_AMT_LCL RET_MANUAL_MKDN_AMT_LCL,
                -RET_MANUAL_MKUP_AMT_LCL RET_MANUAL_MKUP_AMT_LCL,
-               BBG_RETAIL_TYPE_ID
+               BBG_RETAIL_TYPE_IDN
           FROM BBG_RA_SLS_TRX_JL_V@RA_JL
          WHERE DAY_DT = &DAY_DT) T,
        (SELECT DISTINCT A.PROD_NUM, A.PROD_CAT5
