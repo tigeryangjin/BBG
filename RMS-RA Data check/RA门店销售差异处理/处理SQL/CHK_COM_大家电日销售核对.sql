f31b52bb59db2958132038c26faff99a2de24144
tigeryangjin <tigeryangjin@gmail.com>
2016-03-23 08:27:42 +0800

    backup

---
+++ RMS-RA Data check/RA门店销售差异处理/处理SQL/CHK_COM_大家电日销售核对.sql
@@ -0,0 +1,116 @@
+select T.DAY_DT,
+       /*SUBSTR(P.PROD_CAT5,
+       INSTR(P.PROD_CAT5, '~', 1, 2) + 1,
+       INSTR(P.PROD_CAT5, '~', 1, 3) -
+       (INSTR(P.PROD_CAT5, '~', 1, 2) + 1)) DEPT,*/
+       SUM(T.SLS_QTY - T.RET_QTY) QTY,
+       SUM((T.SLS_AMT_LCL - T.SLS_TAX_AMT_LCL) -
+           (T.RET_AMT_LCL - T.RET_TAX_AMT_LCL)) NOTAX_AMT,
+       SUM(((T.SLS_AMT_LCL - T.SLS_TAX_AMT_LCL) -
+           (T.RET_AMT_LCL - T.RET_TAX_AMT_LCL)) -
+           (T.SLS_PROFIT_AMT_LCL - T.RET_PROFIT_AMT_LCL)) COST
+  from (SELECT SLS_TRX_ID,
+               PROD_IT_NUM,
+               ORG_NUM,
+               DAY_DT,
+               VOUCHER_ID,
+               RTL_TYPE_CODE,
+               MIN_NUM,
+               SLS_QTY,
+               SLS_AMT_LCL,
+               SLS_PROFIT_AMT_LCL,
+               SLS_TAX_AMT_LCL,
+               SLS_EMP_DISC_AMT_LCL,
+               SLS_MANUAL_COUNT,
+               SLS_SCAN_COUNT,
+               -RET_QTY                 RET_QTY,
+               -RET_AMT_LCL             RET_AMT_LCL,
+               -RET_PROFIT_AMT_LCL      RET_PROFIT_AMT_LCL,
+               -RET_TAX_AMT_LCL         RET_TAX_AMT_LCL,
+               -RET_EMP_DISC_AMT_LCL    RET_EMP_DISC_AMT_LCL,
+               -RET_MANUAL_COUNT        RET_MANUAL_COUNT,
+               -RET_SCAN_COUNT          RET_SCAN_COUNT,
+               SLS_MANUAL_MKDN_AMT_LCL,
+               SLS_MANUAL_MKUP_AMT_LCL,
+               -RET_MANUAL_MKDN_AMT_LCL RET_MANUAL_MKDN_AMT_LCL,
+               -RET_MANUAL_MKUP_AMT_LCL RET_MANUAL_MKUP_AMT_LCL,
+               BBG_RETAIL_TYPE_ID
+          FROM BBG_RA_SLS_TRX_JL_V@RA_JL
+         WHERE DAY_DT BETWEEN &BDAY_DT AND &EDAY_DT) T,
+       (SELECT DISTINCT A.PROD_NUM, A.PROD_CAT5
+          FROM BBG_RA_PRODUCT_JL_V@RA_JL A) P
+ WHERE T.PROD_IT_NUM = P.PROD_NUM
+ GROUP BY T.DAY_DT /*,
+          SUBSTR(P.PROD_CAT5,
+                 INSTR(P.PROD_CAT5, '~', 1, 2) + 1,
+                 INSTR(P.PROD_CAT5, '~', 1, 3) -
+                 (INSTR(P.PROD_CAT5, '~', 1, 2) + 1))*/
+ ORDER BY T.DAY_DT /*,
+          SUBSTR(P.PROD_CAT5,
+                 INSTR(P.PROD_CAT5, '~', 1, 2) + 1,
+                 INSTR(P.PROD_CAT5, '~', 1, 3) -
+                 (INSTR(P.PROD_CAT5, '~', 1, 2) + 1))*/
+;
+
+--部门-单品-天        
+select T.DAY_DT,
+       t.org_num,
+       t.prod_it_num,
+       /*SUBSTR(P.PROD_CAT5,
+       INSTR(P.PROD_CAT5, '~', 1, 2) + 1,
+       INSTR(P.PROD_CAT5, '~', 1, 3) -
+       (INSTR(P.PROD_CAT5, '~', 1, 2) + 1)) DEPT,*/
+       SUM(T.SLS_QTY - T.RET_QTY) QTY,
+       SUM((T.SLS_AMT_LCL - T.SLS_TAX_AMT_LCL) -
+           (T.RET_AMT_LCL - T.RET_TAX_AMT_LCL)) NOTAX_AMT,
+       SUM(((T.SLS_AMT_LCL - T.SLS_TAX_AMT_LCL) -
+           (T.RET_AMT_LCL - T.RET_TAX_AMT_LCL)) -
+           (T.SLS_PROFIT_AMT_LCL - T.RET_PROFIT_AMT_LCL)) COST
+  from (SELECT SLS_TRX_ID,
+               PROD_IT_NUM,
+               ORG_NUM,
+               DAY_DT,
+               VOUCHER_ID,
+               RTL_TYPE_CODE,
+               MIN_NUM,
+               SLS_QTY,
+               SLS_AMT_LCL,
+               SLS_PROFIT_AMT_LCL,
+               SLS_TAX_AMT_LCL,
+               SLS_EMP_DISC_AMT_LCL,
+               SLS_MANUAL_COUNT,
+               SLS_SCAN_COUNT,
+               -RET_QTY                 RET_QTY,
+               -RET_AMT_LCL             RET_AMT_LCL,
+               -RET_PROFIT_AMT_LCL      RET_PROFIT_AMT_LCL,
+               -RET_TAX_AMT_LCL         RET_TAX_AMT_LCL,
+               -RET_EMP_DISC_AMT_LCL    RET_EMP_DISC_AMT_LCL,
+               -RET_MANUAL_COUNT        RET_MANUAL_COUNT,
+               -RET_SCAN_COUNT          RET_SCAN_COUNT,
+               SLS_MANUAL_MKDN_AMT_LCL,
+               SLS_MANUAL_MKUP_AMT_LCL,
+               -RET_MANUAL_MKDN_AMT_LCL RET_MANUAL_MKDN_AMT_LCL,
+               -RET_MANUAL_MKUP_AMT_LCL RET_MANUAL_MKUP_AMT_LCL,
+               BBG_RETAIL_TYPE_ID
+          FROM BBG_RA_SLS_TRX_JL_V@RA_JL
+         WHERE DAY_DT = &DAY_DT) T,
+       (SELECT DISTINCT A.PROD_NUM, A.PROD_CAT5
+          FROM BBG_RA_PRODUCT_JL_V@RA_JL A) P
+ WHERE T.PROD_IT_NUM = P.PROD_NUM
+/*   and SUBSTR(P.PROD_CAT5,
+              INSTR(P.PROD_CAT5, '~', 1, 2) + 1,
+              INSTR(P.PROD_CAT5, '~', 1, 3) -
+              (INSTR(P.PROD_CAT5, '~', 1, 2) + 1)) = 73*/
+ GROUP BY T.DAY_DT, t.org_num, t.prod_it_num
+/*,
+SUBSTR(P.PROD_CAT5,
+       INSTR(P.PROD_CAT5, '~', 1, 2) + 1,
+       INSTR(P.PROD_CAT5, '~', 1, 3) -
+       (INSTR(P.PROD_CAT5, '~', 1, 2) + 1))*/
+ ORDER BY T.DAY_DT, t.org_num, t.prod_it_num
+/*,
+SUBSTR(P.PROD_CAT5,
+       INSTR(P.PROD_CAT5, '~', 1, 2) + 1,
+       INSTR(P.PROD_CAT5, '~', 1, 3) -
+       (INSTR(P.PROD_CAT5, '~', 1, 2) + 1))*/
+;
--- Task/2016-01-27-购物篮关联分析/购物篮关联分析.sql
+++ Task/2016-01-27-购物篮关联分析/购物篮关联分析.sql
@@ -1,42 +1,47 @@
 
-
 --VIEW
-SELECT * FROM RADM.BBG_RA_SLS_MBA_LC_DY_FV T WHERE T.DT_WID=120160301000;
-SELECT * FROM RADM.BBG_RA_SLS_MBA_DY_FV T WHERE T.DT_WID=120160301000;
-
---RA
-select *
-  from RADM.BBG_RA_SLS_MBA_LC_DY_FV A, RADM.W_MCAL_DAY_DV B
- WHERE A.DT_WID = B.ROW_WID
-   AND B.MCAL_CAL_WID = 1.0
-   AND TRUNC(B.MCAL_DAY_DT) = TO_DATE('2016-03-01', 'YYYY-MM-DD')
-   AND '2010' < B.CAL_YEAR;
+SELECT *
+  FROM RABATCHER.BBG_RA_SLS_MBA_LC_DY_FV T
+ WHERE T.DT_WID = 120160301000;
 
 --CREATE TABLE 
 --BBG_RA_SLS_MBA_LC_DY_TMP
 CREATE TABLE RABATCHER.BBG_RA_SLS_MBA_LC_DY_TMP AS
-SELECT * FROM RABATCHER.BBG_RA_SLS_MBA_LC_DY_FV T WHERE T.DT_WID=120160301000;
+  SELECT *
+    FROM RABATCHER.BBG_RA_SLS_MBA_LC_DY_FV T
+   WHERE T.DT_WID = 120160301000;
 --BBG_RA_SLS_MBA_LC_DY_A
 CREATE TABLE RADM.BBG_RA_SLS_MBA_LC_DY_A AS
-SELECT * FROM RABATCHER.BBG_RA_SLS_MBA_LC_DY_TMP;
+  SELECT * FROM RABATCHER.BBG_RA_SLS_MBA_LC_DY_TMP;
 --sequence
-create sequence BBG_RA_SLS_MBA_LC_DY_A_SEQ
-minvalue 1
-maxvalue 9999999999999999999999999999
-start with 1
-increment by 1
-cache 20;
+create sequence BBG_RA_SLS_MBA_LC_DY_A_SEQ minvalue 1 maxvalue 9999999999999999999999999999 start
+  with 1 increment by 1 cache 20;
 
 --BBG_RA_SLS_MBA_DY_A
-CREATE TABLE RADM.BBG_RA_SLS_MBA_DY_A AS 
-
-
+CREATE TABLE RADM.BBG_RA_SLS_MBA_DY_A AS
+  SELECT T.DT_WID,
+         T.A_PROD_NUM,
+         T.A_PROD_NAME,
+         T.B_PROD_NUM,
+         T.B_PROD_NAME,
+         SUM(T.A_CNT) A_CNT,
+         SUM(T.B_CNT) B_CNT,
+         SUM(T.AB_CNT) AB_CNT,
+         SUM(T.LOC_CNT) LOC_CNT
+    FROM RABATCHER.BBG_RA_SLS_MBA_LC_DY_TMP T
+   GROUP BY T.DT_WID,
+            T.A_PROD_NUM,
+            T.A_PROD_NAME,
+            T.B_PROD_NUM,
+            T.B_PROD_NAME;
+--sequence
+create sequence BBG_RA_SLS_MBA_DY_A_SEQ minvalue 1 maxvalue 9999999999999999999999999999 start
+  with 1 increment by 1 cache 20;
 
 --ODI
---interface1:source：RABATCHER.BBG_RA_SLS_MBA_LC_DY_FV           target:RABATCHER.BBG_RA_SLS_MBA_LC_DY_TMP
---interface2:source：RABATCHER.BBG_RA_SLS_MBA_LC_DY_TMP     target:RADM.BBG_RA_SLS_MBA_LC_DY_A
---interface3:source：RABATCHER.BBG_RA_SLS_MBA_LC_DY_TMP     target:RADM.BBG_RA_SLS_MBA_DY_A
-
+--interface1:source：RABATCHER.BBG_RA_SLS_MBA_LC_DY_FV           target:RABATCHER.BBG_RA_SLS_MBA_LC_DY_TMP   done
+--interface2:source：RABATCHER.BBG_RA_SLS_MBA_LC_DY_TMP          target:RADM.BBG_RA_SLS_MBA_LC_DY_A          done
+--interface3:source：RABATCHER.BBG_RA_SLS_MBA_LC_DY_TMP          target:RADM.BBG_RA_SLS_MBA_DY_A             done
 
 --------------------
 --CN_Retail_As-Was_BBG_Sales_Market_Basket_Analysis
@@ -58,4 +63,192 @@ CREATE TABLE RADM.BBG_RA_SLS_MBA_DY_A AS
 --CN_Retail_As-Was_BBG_Sales_Market_Basket_Analysis_LOC_CNT
 --CD_Retail_As-Was_BBG_Sales_Market_Basket_Analysis_LOC_CNT
 
-SELECT * FROM RADM.W_LOCALIZED_STRING_G T WHERE T.MSG_NUM LIKE '%Retail_As-Was_BBG_VIP_RFM_Analysis' FOR UPDATE;
+SELECT *
+  FROM RADM.W_LOCALIZED_STRING_G T
+ WHERE T.MSG_NUM LIKE '%Retail_As-Was_BBG_VIP_RFM_Analysis'
+   FOR UPDATE;
+
+--***************************************************************************************************************
+--历史数据补入
+--数据从2016.1.1开始
+--***************************************************************************************************************
+--BBG_RA_SLS_MBA_LC_DY_TMP
+/* DETECTION_STRATEGY = NOT_EXISTS */
+insert into RABATCHER.BBG_RA_SLS_MBA_LC_DY_TMP T
+  (DT_WID,
+   ORG_WID,
+   ORG_DH_WID,
+   ORG_SCD1_WID,
+   A_PROD_NUM,
+   A_PROD_NAME,
+   B_PROD_NUM,
+   B_PROD_NAME,
+   A_CNT,
+   B_CNT,
+   AB_CNT,
+   LOC_CNT)
+  select BBG_RA_SLS_MBA_LC_DY_FV.DT_WID,
+         BBG_RA_SLS_MBA_LC_DY_FV.ORG_WID,
+         BBG_RA_SLS_MBA_LC_DY_FV.ORG_DH_WID,
+         BBG_RA_SLS_MBA_LC_DY_FV.ORG_SCD1_WID,
+         BBG_RA_SLS_MBA_LC_DY_FV.A_PROD_NUM,
+         BBG_RA_SLS_MBA_LC_DY_FV.A_PROD_NAME,
+         BBG_RA_SLS_MBA_LC_DY_FV.B_PROD_NUM,
+         BBG_RA_SLS_MBA_LC_DY_FV.B_PROD_NAME,
+         BBG_RA_SLS_MBA_LC_DY_FV.A_CNT,
+         BBG_RA_SLS_MBA_LC_DY_FV.B_CNT,
+         BBG_RA_SLS_MBA_LC_DY_FV.AB_CNT,
+         BBG_RA_SLS_MBA_LC_DY_FV.LOC_CNT
+    from RABATCHER.BBG_RA_SLS_MBA_LC_DY_FV BBG_RA_SLS_MBA_LC_DY_FV
+   where (1 = 1)
+     And (BBG_RA_SLS_MBA_LC_DY_FV.DT_WID BETWEEN &B_DTWID AND &E_DTWID);
+COMMIT;
+--BBG_RA_SLS_MBA_LC_DY_A
+merge /*+ APPEND */
+into RADM.BBG_RA_SLS_MBA_LC_DY_A T
+using (Select /*+ APPEND */
+        BBG_RA_SLS_MBA_LC_DY_TMP.DT_WID DT_WID,
+        BBG_RA_SLS_MBA_LC_DY_TMP.ORG_WID ORG_WID,
+        BBG_RA_SLS_MBA_LC_DY_TMP.ORG_DH_WID ORG_DH_WID,
+        BBG_RA_SLS_MBA_LC_DY_TMP.ORG_SCD1_WID ORG_SCD1_WID,
+        BBG_RA_SLS_MBA_LC_DY_TMP.A_PROD_NUM A_PROD_NUM,
+        BBG_RA_SLS_MBA_LC_DY_TMP.A_PROD_NAME A_PROD_NAME,
+        BBG_RA_SLS_MBA_LC_DY_TMP.B_PROD_NUM B_PROD_NUM,
+        BBG_RA_SLS_MBA_LC_DY_TMP.B_PROD_NAME B_PROD_NAME,
+        BBG_RA_SLS_MBA_LC_DY_TMP.A_CNT A_CNT,
+        BBG_RA_SLS_MBA_LC_DY_TMP.B_CNT B_CNT,
+        BBG_RA_SLS_MBA_LC_DY_TMP.AB_CNT AB_CNT,
+        BBG_RA_SLS_MBA_LC_DY_TMP.LOC_CNT LOC_CNT,
+        SYSDATE W_INSERT_DT,
+        SYSDATE W_UPDATE_DT,
+        BBG_RA_SLS_MBA_LC_DY_TMP.DT_WID || '~' ||
+        BBG_RA_SLS_MBA_LC_DY_TMP.ORG_WID || '~' ||
+        BBG_RA_SLS_MBA_LC_DY_TMP.A_PROD_NUM || '~' ||
+        BBG_RA_SLS_MBA_LC_DY_TMP.B_PROD_NUM INTEGRATION_ID,
+        '1' DATASOURCE_NUM_ID
+         From RABATCHER.BBG_RA_SLS_MBA_LC_DY_TMP BBG_RA_SLS_MBA_LC_DY_TMP
+        Where (1 = 1)) S
+on (T.DT_WID = S.DT_WID and T.ORG_WID = S.ORG_WID and T.A_PROD_NUM = S.A_PROD_NUM and T.B_PROD_NUM = S.B_PROD_NUM)
+when matched then
+  update
+     set T.A_PROD_NAME       = S.A_PROD_NAME,
+         T.B_PROD_NAME       = S.B_PROD_NAME,
+         T.A_CNT             = S.A_CNT,
+         T.B_CNT             = S.B_CNT,
+         T.AB_CNT            = S.AB_CNT,
+         T.LOC_CNT           = S.LOC_CNT,
+         T.W_UPDATE_DT       = S.W_UPDATE_DT,
+         T.INTEGRATION_ID    = S.INTEGRATION_ID,
+         T.DATASOURCE_NUM_ID = S.DATASOURCE_NUM_ID
+when not matched then
+  insert
+    (T.ROW_WID,
+     T.DT_WID,
+     T.ORG_WID,
+     T.ORG_DH_WID,
+     T.ORG_SCD1_WID,
+     T.A_PROD_NUM,
+     T.A_PROD_NAME,
+     T.B_PROD_NUM,
+     T.B_PROD_NAME,
+     T.A_CNT,
+     T.B_CNT,
+     T.AB_CNT,
+     T.LOC_CNT,
+     T.W_INSERT_DT,
+     T.W_UPDATE_DT,
+     T.INTEGRATION_ID,
+     T.DATASOURCE_NUM_ID)
+  values
+    (RADM.BBG_RA_SLS_MBA_LC_DY_A_SEQ.nextval,
+     S.DT_WID,
+     S.ORG_WID,
+     S.ORG_DH_WID,
+     S.ORG_SCD1_WID,
+     S.A_PROD_NUM,
+     S.A_PROD_NAME,
+     S.B_PROD_NUM,
+     S.B_PROD_NAME,
+     S.A_CNT,
+     S.B_CNT,
+     S.AB_CNT,
+     S.LOC_CNT,
+     S.W_INSERT_DT,
+     S.W_UPDATE_DT,
+     S.INTEGRATION_ID,
+     S.DATASOURCE_NUM_ID);
+COMMIT;
+--BBG_RA_SLS_MBA_DY_A
+merge /*+ APPEND */
+into RADM.BBG_RA_SLS_MBA_DY_A T
+using (Select /*+ APPEND */
+        BBG_RA_SLS_MBA_LC_DY_TMP.DT_WID DT_WID,
+        BBG_RA_SLS_MBA_LC_DY_TMP.A_PROD_NUM A_PROD_NUM,
+        BBG_RA_SLS_MBA_LC_DY_TMP.A_PROD_NAME A_PROD_NAME,
+        BBG_RA_SLS_MBA_LC_DY_TMP.B_PROD_NUM B_PROD_NUM,
+        BBG_RA_SLS_MBA_LC_DY_TMP.B_PROD_NAME B_PROD_NAME,
+        SUM(BBG_RA_SLS_MBA_LC_DY_TMP.A_CNT) A_CNT,
+        SUM(BBG_RA_SLS_MBA_LC_DY_TMP.B_CNT) B_CNT,
+        SUM(BBG_RA_SLS_MBA_LC_DY_TMP.AB_CNT) AB_CNT,
+        SUM(BBG_RA_SLS_MBA_LC_DY_TMP.LOC_CNT) LOC_CNT,
+        SYSDATE W_INSERT_DT,
+        SYSDATE W_UPDATE_DT,
+        BBG_RA_SLS_MBA_LC_DY_TMP.DT_WID || '~' ||
+        BBG_RA_SLS_MBA_LC_DY_TMP.A_PROD_NUM || '~' ||
+        BBG_RA_SLS_MBA_LC_DY_TMP.B_PROD_NUM INTEGRATION,
+        1 DATASOURCE_NUM_ID
+         From RABATCHER.BBG_RA_SLS_MBA_LC_DY_TMP BBG_RA_SLS_MBA_LC_DY_TMP
+        Where (1 = 1)
+        Group By BBG_RA_SLS_MBA_LC_DY_TMP.DT_WID,
+                 BBG_RA_SLS_MBA_LC_DY_TMP.A_PROD_NUM,
+                 BBG_RA_SLS_MBA_LC_DY_TMP.A_PROD_NAME,
+                 BBG_RA_SLS_MBA_LC_DY_TMP.B_PROD_NUM,
+                 BBG_RA_SLS_MBA_LC_DY_TMP.B_PROD_NAME,
+                 BBG_RA_SLS_MBA_LC_DY_TMP.DT_WID || '~' ||
+                 BBG_RA_SLS_MBA_LC_DY_TMP.A_PROD_NUM || '~' ||
+                 BBG_RA_SLS_MBA_LC_DY_TMP.B_PROD_NUM
+       ) S
+on (T.DT_WID = S.DT_WID and T.A_PROD_NUM = S.A_PROD_NUM and T.B_PROD_NUM = S.B_PROD_NUM)
+when matched then
+  update
+     set T.A_CNT       = S.A_CNT,
+         T.B_CNT       = S.B_CNT,
+         T.AB_CNT      = S.AB_CNT,
+         T.LOC_CNT     = S.LOC_CNT,
+         T.W_UPDATE_DT = S.W_UPDATE_DT
+when not matched then
+  insert
+    (
+     T.ROW_WID,
+     T.DT_WID,
+     T.A_PROD_NUM,
+     T.A_PROD_NAME,
+     T.B_PROD_NUM,
+     T.B_PROD_NAME,
+     T.A_CNT,
+     T.B_CNT,
+     T.AB_CNT,
+     T.LOC_CNT,
+     T.W_INSERT_DT,
+     T.W_UPDATE_DT,
+     T.INTEGRATION,
+     T.DATASOURCE_NUM_ID
+     )
+  values
+    (
+     RADM.BBG_RA_SLS_MBA_DY_A_SEQ.nextval,
+     S.DT_WID,
+     S.A_PROD_NUM,
+     S.A_PROD_NAME,
+     S.B_PROD_NUM,
+     S.B_PROD_NAME,
+     S.A_CNT,
+     S.B_CNT,
+     S.AB_CNT,
+     S.LOC_CNT,
+     S.W_INSERT_DT,
+     S.W_UPDATE_DT,
+     S.INTEGRATION,
+     S.DATASOURCE_NUM_ID
+     );
+COMMIT;
